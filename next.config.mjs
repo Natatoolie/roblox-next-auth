4940cbd4f81bd66ffebadcfa4e4e0810a31fefbb
Natatouille <47482840+Natatoolie@users.noreply.github.com>
2024-11-03 23:08:54 +0000

    runtime

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,5 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-	experimental: {
-		runtime: "edge",
-	},
-};
+export const runtime = "edge";
+const nextConfig = {};
 
 export default nextConfig;
