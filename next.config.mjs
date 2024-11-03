baffae5d16063d49cbb7f1857e24fb6fb083e433
Natatouille <47482840+Natatoolie@users.noreply.github.com>
2024-11-03 23:31:31 +0000

    e

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,11 @@
 /** @type {import('next').NextConfig} */
 import { setupDevPlatform } from "@cloudflare/next-on-pages/next-dev";
 
-const nextConfig = {};
+const nextConfig = {
+	experimental: {
+		runtime: "edge",
+	},
+};
 
 if (process.env.NODE_ENV === "development") {
 	await setupDevPlatform();
